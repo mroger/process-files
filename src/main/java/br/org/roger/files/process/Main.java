4df6b4197a8133274363eb5c8a91ee9986a871df
Marcos R Oliveira <mroger.oliveira@gmail.com>
2017-11-16 21:57:10 -0200

    Inserting code for profiling

--- src/main/java/br/org/roger/files/process/Main.java
+++ src/main/java/br/org/roger/files/process/Main.java
@@ -1,15 +1,11 @@
 package br.org.roger.files.process;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,7 +14,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
@@ -26,11 +21,6 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.log4j.Logger;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.google.gson.Gson;
 
@@ -41,25 +31,14 @@ import com.google.gson.Gson;
  */
 public class Main {
 	
-	private static final int NO_LINE = 0;
-	private static final int ONE_LINE = 1;
-	private static final String HTTP_METHOD_POST = "POST";
-	private static final String API_ENDPOINT = "https://localhost:8080/inbox";
-	private static final int TIMEOUT = 2000;
-
 	private final String filesPath;
 	private boolean encryptEnabled;
-	private boolean hasHeader;
-	private boolean postToEndpoint;
-	private String endpointUrl = API_ENDPOINT;
 	
 	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 	
 	private Main(final MainBuilder builder) {
 		this.filesPath = builder.getFilesPath();
 		this.encryptEnabled = builder.encryptEnabled();
-		this.hasHeader = builder.hasHeader();
-		this.postToEndpoint = builder.postToEndpoint();
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -71,29 +50,32 @@ public class Main {
 		Main main = new Main.MainBuilder(args)
 			.withFilesPath()
 			.withEncryptEnabled()
-			.withHasHeader()
-			.withPostToEndpoint()
 			.build();
 		main.processFiles();
 		
 	}
 	
 	private void processFiles() {
+		long start = System.currentTimeMillis();
+		
 		Path inputFilesDir = Paths.get(this.filesPath);
+		long quant = 0;
 		try (Stream<Path> files = Files.list(inputFilesDir)) {
-			files
+			quant = files
 				.filter(Files::isRegularFile)
 				.map(Path::toFile)
-				.flatMap(this::extractRowsFromFile)
-				.map(this::mapToDomain)
+				.flatMap(this::convertToMobileData)
 				.collect(Collectors.groupingBy(DomainObject::getHash, Collectors.toList())).values().stream()
 				.flatMap(this::calculateLastCoordinates)
 				.map(this::convertToJson)
 				.map(this::encryptJson)
 				.filter(this::isNonEmptyData)
 				.map(this::getNonEmptyData)
-				.peek(System.out::println)
-				.forEach(this::postMessage);
+				.count();
+			
+			long now = System.currentTimeMillis();
+	        System.out.println((now - start) / 1000.0 + " - Quant: " + quant);
+			
 			System.exit(0);
 		} catch (Exception e) {
 			LOGGER.error("Error listing files from path.", e);
@@ -101,26 +83,31 @@ public class Main {
 		}
 	}
 	
-	private Stream<Row> extractRowsFromFile(final File inputFile) {
-		int skipLines = hasHeader ? ONE_LINE : NO_LINE;
-		boolean isParallel = false;
-		try(XSSFWorkbook workbook = new XSSFWorkbook(inputFile)) {
-			XSSFSheet spreadsheet = workbook.getSheetAt(0);
-			return StreamSupport.stream(spreadsheet.spliterator(), isParallel)
-				.skip(skipLines);
-		} catch (InvalidFormatException | IOException e) {
-			LOGGER.error("Error processing file " + inputFile.getAbsolutePath(), e);
+	private Stream<DomainObject> convertToMobileData(final File mobileFile) {
+		try (InputStream inputStream = new FileInputStream(mobileFile);
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));) {
+			return bufferedReader
+				.lines()
+				.map(this::toArray)
+				.map(this::mapToDomain)
+				.collect(Collectors.toList()).stream();
+		} catch (Exception e) {
+			LOGGER.error("Error processing file " + mobileFile.getAbsolutePath(), e);
 			return Stream.empty();
 		}
 	}
 
-	private DomainObject mapToDomain(final Row row) {
+	private String[] toArray(final String line) {
+		return line.split("\t");
+	}
+	
+	private DomainObject mapToDomain(final String[] fields) {
 		return new DomainObject(
-			getCheckedValue(row.getCell(0)),
-			getCheckedValue(row.getCell(1)),
-			getCheckedValue(row.getCell(2)),
-			getCheckedValue(row.getCell(3)),
-			getCheckedValue(row.getCell(4)));
+			fields[1],
+			fields[0],
+			fields[3],
+			fields[4],
+			fields[6]);
 	}
 	
 	private Stream<DomainObject> calculateLastCoordinates(final List<DomainObject> domainValue) {
@@ -141,20 +128,6 @@ public class Main {
 		return i == 0;
 	}
 
-	private String getCheckedValue(Cell cell) {
-		if (cell == null) {
-			return "";
-		}
-		switch(cell.getCellTypeEnum()) {
-			case NUMERIC:
-				return String.valueOf(cell.getNumericCellValue());
-			case STRING:
-				return cell.getStringCellValue();
-			default:
-				return "";
-		}
-	}
-	
 	private String convertToJson(final DomainObject mobileData) {
 		return new Gson().toJson(mobileData);
 	}
@@ -189,45 +162,10 @@ public class Main {
 		return nonEmptyData.get();
 	}
 	
-	private void postMessage(final String jsonToSend) {
-		if (!postToEndpoint) {
-			return;
-		}
-		try {
-			URL url = new URL(this.endpointUrl);
-
-			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-			urlConnection.setDoOutput(true);
-			urlConnection.setConnectTimeout(TIMEOUT);
-			urlConnection.setRequestMethod(HTTP_METHOD_POST);
-
-			try (OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
-					InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-					BufferedReader r = new BufferedReader(new InputStreamReader(in));) {
-
-				out.write(jsonToSend.getBytes());
-
-				String line;
-				StringBuilder sb = new StringBuilder();
-				while ((line = r.readLine()) != null) {
-					sb.append(line).append('\n');
-				}
-
-				LOGGER.info("Data sent to the endpoint!");
-			} catch (Exception e) {
-				LOGGER.error("Error! It's not possible to send data to the server.", e);
-			}
-		} catch (Exception e) {
-			LOGGER.error("Error! It's not possible to connect to the server.", e);
-		}
-	}
-	
 	private static class MainBuilder {
 		private String[] args;
 		private String filesPath;
 		private boolean encryptEnabled = false;
-		private boolean hasHeader = true;
-		private boolean postToEndpoint = false;
 		
 		public MainBuilder(String[] args) {
 			this.args = args;
@@ -245,20 +183,6 @@ public class Main {
 			return this;
 		}
 		
-		public MainBuilder withHasHeader() {
-			if (args.length > 2) {
-				this.hasHeader = Boolean.valueOf(args[2]);
-			}
-			return this;
-		}
-		
-		public MainBuilder withPostToEndpoint() {
-			if (args.length > 3) {
-				this.postToEndpoint = Boolean.valueOf(args[3]);
-			}
-			return this;
-		}
-		
 		public Main build() {
 			return new Main(this);
 		}
@@ -270,16 +194,6 @@ public class Main {
 		public boolean encryptEnabled() {
 			return encryptEnabled;
 		}
-
-		public boolean hasHeader() {
-			return hasHeader;
-		}
-
-		public boolean postToEndpoint() {
-			return postToEndpoint;
-		}
-		
-		
 	}
 
 }
